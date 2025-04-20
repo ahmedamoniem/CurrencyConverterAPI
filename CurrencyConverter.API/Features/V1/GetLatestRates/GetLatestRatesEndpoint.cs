0db75293b53a9833d8112fef8baa0fc4316ec56f
Ahmed A.Moniem Hassan <ahmed.joum@outlook.com>
2025-04-20 17:47:51 +0400

    Adding GetLatestRateEndpoint Adding TestStartup Helper

---
+++ CurrencyConverter.API/Features/V1/GetLatestRates/GetLatestRatesEndpoint.cs
@@ -0,0 +1,26 @@
+﻿using CurrencyConverter.Application.DTOs;
+using CurrencyConverter.Application.Interfaces;
+using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CurrencyConverter.API.Features.V1.GetLatestRates;
+
+
+[Authorize(Roles = "viewer,admin")]
+public class GetLatestRatesEndpoint(ICurrencyService currencyService) : Endpoint<GetLatestRatesRequest, ExchangeRateDto>
+{
+    private readonly ICurrencyService _currencyService = currencyService;
+
+    public override void Configure()
+    {
+        Verbs(Http.GET);
+        Routes("/api/v{version:apiVersion}/rates/latest");
+        Version(1);
+    }
+
+    public override async Task HandleAsync(GetLatestRatesRequest req, CancellationToken ct)
+    {
+        var result = await _currencyService.GetLatestRatesAsync(req.BaseCurrency);
+        await SendOkAsync(result, ct);
+    }
+}
