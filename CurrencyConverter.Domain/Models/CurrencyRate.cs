dd303f6ae89f0509690f790ed35d08f104f15e79
Ahmed A.Moniem Hassan <ahmed.joum@outlook.com>
2025-04-20 15:55:44 +0400

    adding domain models

---
+++ CurrencyConverter.Domain/Models/CurrencyRate.cs
@@ -0,0 +1,9 @@
+﻿namespace CurrencyConverter.Domain.Models;
+
+public class CurrencyRate
+{
+    public string BaseCurrency { get; set; } = default!;
+    public string TargetCurrency { get; set; } = default!;
+    public decimal Rate { get; set; }
+    public DateTime Date { get; set; }
+}
