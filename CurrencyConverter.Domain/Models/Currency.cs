9890a26faa9da7d4c8c0c4292a6ceac2d5684159
Ahmed A.Moniem Hassan <ahmed.joum@outlook.com>
2025-04-21 04:46:35 +0400

    removing domain models

--- CurrencyConverter.Domain/Models/Currency.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CurrencyConverter.Domain.Models;
-
-public class Currency
-{
-    public string Code { get; set; } = default!;
-    public string Name { get; set; } = default!;
-    public string Symbol { get; set; } = default!;
-}
--- CurrencyConverter.Domain/Models/CurrencyRate.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CurrencyConverter.Domain.Models;
-
-public class CurrencyRate
-{
-    public string BaseCurrency { get; set; } = default!;
-    public string TargetCurrency { get; set; } = default!;
-    public decimal Rate { get; set; }
-    public DateTime Date { get; set; }
-}
